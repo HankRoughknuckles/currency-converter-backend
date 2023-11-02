6cc2f3c8f4fb30fdadd915ba7e80a303c2945e01
Thomas Morris <tmorris@amp.energy>
2023-11-02 11:37:34 +0100

    1 - Make CORS proxy backend
    CORS does not allow our frontend to call the CNB api, since the CNB api
    only allows CORS from "apl.cnb.cz" (its own site).
    
    Therefore, I made this backend that will call the CNB API (since there's
    no CORS issue there), and made this endpoint callable from our frontend
    (specified in the CORS_ALLOW env var).
    
    Also, the cnb.service.ts service calls the api, but then maps it to
    a standardized output.  Therefore if we were to use a different api in
    the future (one that has a different response format), we would be able
    to make a different plugin for that api and make it map to this
    standardized interface.

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin'
+  ],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    browser: true,
+    es2020: true
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  rules: {},
+}
--- index.ts
+++ index.ts
@@ -1,22 +1,27 @@
-import express, { Request, Response , Application } from 'express';
-import dotenv from 'dotenv';
+import express, { Request, Response, Application } from "express";
+import dotenv from "dotenv";
+import { fetchExchangeRates, Rate } from "./src/cnb.service";
 
-//For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  // Set appropriate headers to allow CORS
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+const setCorsHeaders = (res: Response) => {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ALLOW);
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+};
 
-  res.send('Welcome to Express & TypeScript Server');
+app.get("/", async (req: Request, res: Response<Rate[]>) => {
+  const rates = await fetchExchangeRates();
+  setCorsHeaders(res);
+  res.send(rates);
 });
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`CORS proxy server is running on port ${port}`);
 });
-
